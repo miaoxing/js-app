7f81bdaee4415f04b7e716613f6e7abe4ba85de8
twinh <twinhuang@qq.com>
2020-08-31 15:21:24 +0800

    feat: 增加 $.http $.patch $.put $.delete 接口

--- index.ts
+++ index.ts
@@ -33,12 +33,43 @@ const $ = {
     }
   },
 
+  http: async (...args: any[]) => {
+
+  },
   get: async () => {
 
   },
   post: async () => {
 
   },
-}
+  patch: async () => {
+
+  },
+  put: async () => {
+
+  },
+  delete: async () => {
+
+  }
+};
+
+const methods = ['get', 'post', 'patch', 'put', 'delete'];
+methods.forEach((method: string) => {
+  // @ts-ignore
+  $[method] = async (...args: any[]) => {
+    let config;
+
+    if (typeof args[0] === 'string') {
+      config = args[1] || {};
+      config.url = args[0];
+    } else {
+      config = args[0];
+    }
+
+    config.method = method;
+
+    return $.http(config);
+  };
+});
 
 export default $;
