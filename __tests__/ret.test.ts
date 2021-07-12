6c85b2f9f7024bbb053c151625a5dd8df645c926
twinh <twinhuang@qq.com>
2021-07-12 14:21:02 +0800

    test(ret): 增加 `Ret.suc` 和 `Ret.err` 方法用于快速创建 `Ret` 对象

---
+++ __tests__/ret.test.ts
@@ -0,0 +1,56 @@
+import {Ret} from '../';
+
+describe('Ret', () => {
+  test('suc with string message', () => {
+    const ret = Ret.suc('ok');
+    expect(ret.code).toBe(0);
+    expect(ret.message).toBe('ok');
+    expect(ret.isSuc()).toBeTruthy();
+  });
+
+  test('suc with object without code', () => {
+    const ret = Ret.suc({
+      message: 'ok',
+      data: 'test',
+    });
+    expect(ret.code).toBe(0);
+    expect(ret.message).toBe('ok');
+    expect(ret.data).toBe('test');
+    expect(ret.isSuc()).toBeTruthy();
+  });
+
+  test('suc with object without message', () => {
+    const ret = Ret.suc({
+      data: 'test',
+    });
+    expect(ret.code).toBe(0);
+    expect(ret.message).toBe('操作成功');
+    expect(ret.data).toBe('test');
+    expect(ret.isSuc()).toBeTruthy();
+  });
+
+  test('err with string message', () => {
+    const ret = Ret.err('err');
+    expect(ret.code).toBe(-1);
+    expect(ret.message).toBe('err');
+    expect(ret.isSuc()).toBeFalsy();
+  });
+
+  test('err with custom code', () => {
+    const ret = Ret.err('err', 1);
+    expect(ret.code).toBe(1);
+    expect(ret.message).toBe('err');
+    expect(ret.isSuc()).toBeFalsy();
+  });
+
+  test('err with object without code', () => {
+    const ret = Ret.err({
+      message: 'err',
+      data: 'test',
+    });
+    expect(ret.code).toBe(-1);
+    expect(ret.message).toBe('err');
+    expect(ret.data).toBe('test');
+    expect(ret.isSuc()).toBeFalsy();
+  });
+});
